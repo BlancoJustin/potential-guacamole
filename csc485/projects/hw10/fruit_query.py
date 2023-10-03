b91f4d2e89efd7758403e7a0a41f8fa6689ac06c
BlancoJustin <146274265+BlancoJustin@users.noreply.github.com>
2023-10-02 22:01:34 -0400

    Add files via upload

---
+++ csc485/projects/hw10/fruit_query.py
@@ -0,0 +1,12 @@
+
+
+def is_it_a_fruit(fruit_candidate):
+    fruits = ['apple', 'pear', 'bananna', 'grape']
+
+    if fruit_candidate in fruits:
+        return True
+    else:
+        return False
+
+
+  
